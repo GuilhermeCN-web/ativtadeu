9f70ae3cc97185f9ca8976a6c6f97ebfc613be6f
Guilherme <guilherme.narcizo@fatec.sp.gov.br>
2025-09-10 17:31:17 -0300

    :boom: fix: ajuste feito

--- Main.java
+++ Main.java
@@ -7,9 +7,9 @@ public class Main {
         tree.add(new Node(12));
         tree.add(new Node(7));
         tree.add(new Node(3));
+        tree.add(new Node(3));
         tree.add(new Node(2));
         tree.add(new Node(1));
-        tree.add(new Node(3));
         tree.add(new Node(27));
         tree.add(new Node(33));
         tree.add(new Node(75));
